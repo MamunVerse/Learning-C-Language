e5910e84986780a445b59c82f29717c89b6cd8ff
Mamunur Rashid <49125977+MamunVerse@users.noreply.github.com>
2022-10-02 05:00:33 +0600

    variable data type

---
+++ Chapter1/Variable.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+
+int main(){
+    
+    int number = 25;  // int store integet number
+
+    char star = '*'; // car store charecter
+
+    float pi = 3.145; // float store float number
+
+    return 0;
+}
