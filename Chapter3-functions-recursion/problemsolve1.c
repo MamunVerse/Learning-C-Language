c90bacc3b41172b4c05b32c901c2cc1ccc76820d
Mamunur Rashid <49125977+MamunVerse@users.noreply.github.com>
2022-10-02 23:51:02 +0600

    Problem 1 : Write 2 functions thats print Assalamualaikum if user is Bangladeshi, print Namaste if user in Indian & Hello for others.& Namaste if user in Indian.

---
+++ Chapter3-functions-recursion/problemsolve1.c
@@ -0,0 +1,39 @@
+/*
+Problem 1 : Write 2 functions thats print Assalamualaikum if user is Bangladeshi & Namaste if user in Indian.
+*/
+
+# include <stdio.h>
+
+// Function declaration/prototype
+void salam(); 
+void namaste(); 
+
+int main(){
+    
+    printf("Enter b for Bangladeshi & i for Indian : \n");
+    
+    char ch;
+    scanf("%c", &ch);
+
+    //  Conditionaly Function call
+
+    if(ch == 'b'){
+        salam();
+    }else if(ch == 'i'){
+        namaste();
+    }else{
+        printf("Hello \n");
+    }
+
+    return 0;
+}
+
+// Function defination
+void salam(){
+    printf("Assalamualaikum \n");
+}
+void namaste(){
+    printf("Namaste \n");
+}
+
+
