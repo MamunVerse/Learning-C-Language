1a8ea9d847f1e7c0d985d3c026d3af5dcfa93fd9
Mamunur Rashid <49125977+MamunVerse@users.noreply.github.com>
2022-10-02 15:32:17 +0600

    learn printf

---
+++ Chapter1/printf.c
@@ -0,0 +1,20 @@
+# include <stdio.h>
+
+int main(){
+    
+    int age = 25;  // int store integet number
+
+    float piValue = 3.145; // float store float number
+
+    char *name = "Mamunur Rashid"; // char store character
+
+
+    printf("My age is : %d \n", age);  // %d for print integer value
+
+    printf("Value of pi is : %f \n", piValue); // %d  forfloat print integer value
+
+    printf("My name is : %s \n", name); // %s for character print integer value
+
+    return 0;
+}
+
--- Chapter1/variable.c
+++ Chapter1/variable.c
@@ -4,9 +4,10 @@ int main(){
     
     int number = 25;  // int store integet number
 
-    char star = '*'; // car store charecter
+    char star = '*'; // char store character
 
     float pi = 3.145; // float store float number
 
     return 0;
 }
+
