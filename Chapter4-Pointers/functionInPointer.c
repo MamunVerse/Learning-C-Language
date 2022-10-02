5ed55711e62af6c32034d73e8c860086408dd0a4
Mamunur Rashid <49125977+MamunVerse@users.noreply.github.com>
2022-10-03 03:30:19 +0600

    learn function in pointer

---
+++ Chapter4-Pointers/functionInPointer.c
@@ -0,0 +1,29 @@
+# include <stdio.h>
+
+void square(int n);
+void _square(int *n);
+
+int main(){
+    int number = 4;
+
+    square(number);
+    printf("number is  =  %d \n", number);
+
+    _square(&number);
+    printf("number is  =  %d \n", number);
+
+    return 0;
+}
+
+// call by value
+void square(int n){
+    n = n * n;
+    printf("Square =  %d \n", n);
+}
+
+// call by address
+void _square(int *n){
+    *n = (*n) * (*n);
+    printf("Square =  %d \n", *n);
+}
+
