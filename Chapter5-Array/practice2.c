755acde35fd893046cdc5e52babd3a90dde171d0
Mamunur Rashid <49125977+MamunVerse@users.noreply.github.com>
2022-10-04 01:14:09 +0600

    Practice 2 : Write a function to count the number of odd numbers in an array.

--- Chapter5-Array/practice2.c
+++ Chapter5-Array/practice2.c
@@ -5,10 +5,23 @@ Practice 2 : Write a function to count the number of odd numbers in an array.
 
 # include <stdio.h>
 
-int main(){
-    
-    
+int countOdd(int arr[], int n);
 
+int main(){
+    int arr[] = {1,2,3,4,5,6};
+
+    printf("%d\n", countOdd(arr, 6));
     return 0;
 } 
+
+int countOdd(int arr[], int n){
+    int count = 0;
+    for(int i =0; i<n; i++){
+        if(arr[i] % 2 != 0){ // odd number
+            count++;
+        }
+    }
+
+    return count;
+}
 
